e82edd15bbf85bf19b01bcb4760ccfad38a68268
Jaime Diaz <jaime.diaz-gonzalez-external@capgemini.com>
2019-03-08 12:46:01 +0100

    Ready to be merged

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port: string = '5010'; // 5010 is the port for development testing
+  let port: string = process.argv[2];
   app.use(bodyParser.json({ limit: '12mb' }));
   app.use(bodyParser.urlencoded({ limit: '12mb', extended: true }));
+
   console.log('The port is: ' + port);
   await app.listen(port);
 }
